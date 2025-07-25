6ffef37be0a709e52fa3ba574c1d3199018d58d6
dangeno <dgeneau@csipacific.ca>
2025-07-25 07:54:24 -0700

    Data Update

---
+++ prog_pull_official.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+race_lists = pd.read_excel('/Users/danielgeneau/Library/CloudStorage/OneDrive-SharedLibraries-RowingCanadaAviron/HP - Staff - SSSM/General/Performance Analysis/FISA GPS Databases/Sources/races.xlsx')
+results_data = pd.read_excel('/Users/danielgeneau/Library/CloudStorage/OneDrive-SharedLibraries-RowingCanadaAviron/HP - Staff - SSSM/General/Performance Analysis/FISA GPS Databases/Sources/result.xlsx')
+
+print(results_data)
